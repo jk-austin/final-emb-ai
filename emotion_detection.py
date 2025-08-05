1a208c6b0550d944633fa28ee6b1faaeeea07e9d
jesse korff <jessekorff@gmail.com>
2025-08-05 14:11:24 -0400

    extract emotion scores

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,37 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
+    
+    # Sending a POST request to the sentiment analysis API
     response = requests.post(url, json = myobj, headers=header)
-    return response.text
+    
+    # Parsing the JSON response from the API
+    formatted_response = json.loads(response.text)
+
+    #extract emotion scores from the parsed response
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    anger = emotions['anger']
+    disgust = emotions['disgust']
+    fear = emotions['fear']
+    joy = emotions['joy']
+    sadness = emotions['sadness']
+
+    #identify dominant emotion
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotions, key=emotions.get)
+    dominant_score = emotions[dominant_emotion]
+
+
+    return {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion': dominant_emotion,
+        'dominant_score': dominant_score
+    }
